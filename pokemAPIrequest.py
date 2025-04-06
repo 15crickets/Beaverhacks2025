e2812441b8757b0733a176d73f24ed7e663058d5
sc30714 <vikram.vasudevan@comcast.net>
2025-04-05 17:44:58 -0700

    added stuff

--- pokemAPIrequest.py
+++ pokemAPIrequest.py
@@ -5,22 +5,69 @@ from pokemontcgsdk import Set
 import requests
 
 sets = Set.all()
+for item in sets:
+    set_name = item.name  # e.g., "Generations"
+    print("Set Name:", set_name)
+
+    # Create the query with exact casing and quotes
+    query = f'set.name:"{set_name}"'
+    print("Query:", query)
+
+    cards = Card.where(q=query)
+    for card in cards:
+        print(card.name)
+        if card.tcgplayer.prices is not None: 
+            if card.tcgplayer.prices.holofoil is not None:
+                print(card.tcgplayer.prices)
+                print(card.tcgplayer.prices.holofoil.market)
+            else:
+                print(card.tcgplayer.prices)
+
+
+'''
+
+sets = Set.all()
 
 for item in sets:
     print(item.name)
 
+for card in cards:
+    print(card.name)
 
-cards = Card.where(q='set.name:base')
+cards = Card.where(q='set.name:journey_together')
+print("HI")
+for card in cards:
+    print(card.name)
+
+
+
+
+requests library. You need to make a virtual environment 
+to install this library first
+from pokemontcgsdk import Card
+from pokemontcgsdk import Set
+import requests
+
+sets = Set.all()
+
+for item in sets:
+    print(item.name)
+
+template_query = 'set.name:{setName}'
+
+print("Final item name" + item.name)
+name = item.name
+name = name.lower()
+name = name.replace(" ", "")
+print("NAME: " + name)
+
+newString = template_query.format(setName=name)
+print("NEWSTRING: " + newString)
+
+
+cards = Card.where(q='set.name:generations')
 
 for card in cards:
     print(card.name)
 
-"""url = "https://api.pokemontcg.io/v2/cards"
-
-r = requests.get(url)
-data = r.json()
-
-print(data)
-"""
-
-
+'''
